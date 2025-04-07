c3ef7db6331cd57bae889ecc467ad22a6ad4c14e
Daniil Tarashkevich <danilatarashkevich@gmail.com>
2025-04-07 15:54:29 +0300

    CacheUtil fix

--- src/main/java/com/iblochko/notes/util/CacheUtil.java
+++ src/main/java/com/iblochko/notes/util/CacheUtil.java
@@ -2,7 +2,6 @@ package com.iblochko.notes.util;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,28 +9,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class CacheUtil {
     private final Map<String, Object> cache = new ConcurrentHashMap<>();
-    private final AtomicLong cacheHits = new AtomicLong(0);
-    private final AtomicLong cacheMisses = new AtomicLong(0);
+    private final int maxCacheSize;
+
+    CacheUtil() {
+        maxCacheSize = 100;
+    }
 
     public <T> T get(String key, Class<T> type) {
         Object value = cache.get(key);
         if (value != null) {
-            cacheHits.incrementAndGet();
             log.info("Cache hit for key: {}", key);
             return type.cast(value);
         }
-        cacheMisses.incrementAndGet();
         log.info("Cache miss for key: {}", key);
         return null;
     }
 
     public void put(String key, Object value) {
-        int maxCacheSize = 100;
         if (cache.size() >= maxCacheSize) {
             log.info("Cache is full, clearing...");
             this.clear();
-            cacheHits.set(0);
-            cacheMisses.set(0);
         }
         cache.put(key, value);
         log.info("Added to cache: {}", key);
